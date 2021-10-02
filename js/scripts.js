554b2f8050ec292b837aeb2da57ea755cc49896d
Erycson Jose <erycsonjose@gmail.com>
2021-10-02 18:16:42 -0300

    criar arquivo scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+var tituloProduto = document.querySelector('#produto0'); // atribuir uma tag html a variável tituloProduto através do id 
+tituloProduto.textContent = listaProdutos[0].descricao; 
+// var precoProduto = document.querySelector('#preco0');
+// precoProduto.textContent = listaProdutos[0].preco;
