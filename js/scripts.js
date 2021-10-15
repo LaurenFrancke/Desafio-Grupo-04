82b693a8c30dfad16458318c0012a86688ff03ca
Thiagorsfranca <90103752+Thiagorsfranca@users.noreply.github.com>
2021-10-14 21:38:43 -0300

    Update scripts.js
    Adicionando resolução da 12.

--- js/scripts.js
+++ js/scripts.js
@@ -157,6 +157,46 @@ resposta10.textContent = item10();
 // final da questão 10
 
 
+// inicio da 12
+
+function exercicio12(){
+
+    let listaDeptos = [];
+    let codDepto = 0;
+
+    for (i=0;i<lista.length; i++){
+        let produto = lista[i];
+        if (produto.departamento.idDepto != codDepto){
+            let itemLista = {
+                nomeDepto: produto.departamento.nomeDepto,
+                idDepto: produto.departamento.idDepto,
+                inventarioDpto: 0
+            };
+            listaDeptos.push(itemLista);
+            codDepto = produto.departamento.idDepto;
+        }
+    }
+
+
+    for (i=0; i < lista.length ; i++){
+        let produto = lista[i];
+
+        for (j=0;j<listaDeptos.length; j++){
+            if (produto.departamento.idDepto == listaDeptos[j].idDepto){  // o depto do produto corresponde ao depto da lista?
+                listaDeptos[j].inventarioDpto += (produto.qtdEstoque * produto.preco);
+                break;
+            }
+        }
+    }
+
+    console.log(listaDeptos);
+}
+
+var resposta12 = document.querySelector('#resposta12');
+resposta7.textContent = item12();
+
+//final da 12
+
 //inicio da 13
 function item13(){
     var listaDeptos = [];
